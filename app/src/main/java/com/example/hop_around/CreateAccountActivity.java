165cb014acd5d3879e454f8d4982a03f981774fe
Caroline Reynolds <carolinereynolds@carolines-air.home>
2020-04-01 23:15:43 -0400

    Added SetUp for account after intitial creation, still need to do a lot of error handling

--- app/src/main/java/com/example/hop_around/CreateAccountActivity.java
+++ app/src/main/java/com/example/hop_around/CreateAccountActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -28,6 +29,23 @@ public class CreateAccountActivity extends AppCompatActivity {
 
                 Intent myIntent = new Intent(CreateAccountActivity.this, TermsAndServices.class);
                 startActivity(myIntent);
+
+            }
+        });
+
+        // when the Create Account button is pressed
+        Button create = (Button) findViewById(R.id.create_account_btn);
+        create.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //TODO DATABASE @Jerry, need add new account to DB/check that email hasn't already been registered (error handling)
+                    //TODO maybe also have a requirement for passwords, and check that req is met?
+
+                //
+            Intent myIntent = new Intent(CreateAccountActivity.this, SetUpAccountActivity.class);
+            startActivity(myIntent);
 
             }
         });
---
+++ app/src/main/java/com/example/hop_around/SetUpAccountActivity.java
@@ -0,0 +1,78 @@
+package com.example.hop_around;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SetUpAccountActivity extends AppCompatActivity {
+
+    private Button next;
+    private ImageView toEditProfilePhoto;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_account);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Welcome to Hop Around!");
+
+        toEditProfilePhoto = (ImageView) findViewById(R.id.launch_editProfilePhoto);
+        toEditProfilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent editPhoto = new Intent(SetUpAccountActivity.this, EditProfilePhotoActivity.class);
+                startActivity(editPhoto);
+
+            }
+        });
+
+        //TODO @Jerry when user presses next button, need to save info to DB
+            //NOTE: in THIS onclick for next button, it means user did NOT add a profile photo
+        next = (Button) findViewById(R.id.next_btn);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //TODO ensure profile is done being created (values in all the text fields)
+                //TODO then launch into map activity!
+                //Intent next = new Intent(SetUpAccountActivity.this, TODO.class);
+                //startActivity(next);
+            }
+        });
+
+    }
+
+    //TODO @Jerry Database things, this will be called when user returns to the activity after EditProfileActivity.
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        //TODO @Jerry when user presses next button, need to save info to DB
+            //NOTE: in THIS onClick for next button, it means user DID add a profile photo
+        next = (Button) findViewById(R.id.next_btn);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //TODO ensure profile is done being created (values in all the text fields)
+                //TODO then launch into map activity!
+                //Intent next = new Intent(SetUpAccountActivity.this, TODOMapView.class);
+                //startActivity(next);
+            }
+        });
+    }
+
+
+
+}
