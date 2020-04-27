dddb95074d8d6284a58f66e3f5016656261819a6
Jchen <52466356+Flyingsledge@users.noreply.github.com>
2020-04-26 21:53:04 -0400

    collection fragment db implementation

--- app/src/main/java/com/example/hop_around/CollectionFragment.java
+++ app/src/main/java/com/example/hop_around/CollectionFragment.java
@@ -1,18 +1,36 @@
 package com.example.hop_around;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class CollectionFragment extends Fragment {
 
@@ -31,16 +49,29 @@ public class CollectionFragment extends Fragment {
         mPopupList.add(new PopupItem(R.drawable.ic_launcher_background, "Name 2", 1));
         mPopupList.add(new PopupItem(R.drawable.ic_map, "Name 3", 2));
 
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        final String UID = sharedPreferences.getString("UID", "kidPizza");
+        final DatabaseReference dbRoot = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference usersRef = dbRoot.child("users");
+            ValueEventListener swagListener = new ValueEventListener() {
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int b = Math.toIntExact((long) dataSnapshot.child("popCount").getValue());
+                for (int i = 0; i < b; i++) {
+                    if (dataSnapshot.child(UID).hasChild(""+ i)){
+                        String title = (String) dataSnapshot.child("popups").child("" + i).child("title").getValue();
+                        String bitmap = (String) dataSnapshot.child("popups").child("" + i).child("bitmap").getValue();
+                        mPopupList.add(new PopupItem(StringToBitMap(bitmap),  title, i));
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        };
 
-        /*
-
-
-        for (int i = 0; i < ....) {
-            mPopupList.add(new PopupItem(image,  title, id ));
-        }
-        */
-
-
+        usersRef.addListenerForSingleValueEvent(swagListener);
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_collection);
         mRecyclerView.setHasFixedSize(true);
@@ -65,5 +96,16 @@ public class CollectionFragment extends Fragment {
         PopupItem item = mPopupList.get(position);
         intent.putExtra("refId", item.getId());
         startActivity(intent);
+    }
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte = Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }
+        catch(Exception e){
+            e.getMessage();
+            return null;
+        }
     }
 }
